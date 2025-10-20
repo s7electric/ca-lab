04cf5e237596965a1db2f708ecdd56c6f5762ca8
s7electric <samiacobazzi@gmail.com>
2025-10-20 16:39:22 -0400

    Fix null call

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -1,7 +1,9 @@
 package interface_adapter.logout;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInState;
 import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginState;
 import interface_adapter.login.LoginViewModel;
 import use_case.logout.LogoutOutputBoundary;
 import use_case.logout.LogoutOutputData;
@@ -34,13 +36,20 @@ public class LogoutPresenter implements LogoutOutputBoundary {
 
         // TODO: have prepareSuccessView update the LoggedInState
         // 1. get the LoggedInState out of the appropriate View Model,
+        LoggedInState loggedInState = this.loggedInViewModel.getState();
         // 2. set the username in the state to the empty string
+        loggedInState.setUsername("");
         // 3. firePropertyChanged so that the View that is listening is updated.
+        this.loggedInViewModel.firePropertyChange();
 
         // TODO: have prepareSuccessView update the LoginState
         // 1. get the LoginState out of the appropriate View Model,
+        LoginState loginState = this.loginViewModel.getState();
         // 2. set the username in the state to be the username of the user that just logged out,
+        loginState.setUsername(response.getUsername());
+        loginState.setPassword("");
         // 3. firePropertyChanged so that the View that is listening is updated.
+        this.loginViewModel.firePropertyChange();
 
         // This code tells the View Manager to switch to the LoginView.
         this.viewManagerModel.setState(loginViewModel.getViewName());
