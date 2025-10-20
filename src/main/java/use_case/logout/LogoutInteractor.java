be36b056b0da0ba5de968373d6a93a62d0bdbf93
Eric Xu <eric@Erics-MacBook-Pro-100.local>
2025-10-20 15:53:09 -0400

    logoutinteractor.java

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -1,5 +1,7 @@
 package use_case.logout;
 
+import use_case.login.LoginOutputData;
+
 /**
  * The Logout Interactor.
  */
@@ -9,12 +11,16 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
                             LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
+        this.userDataAccessObject = userDataAccessInterface;
+        this.logoutPresenter = logoutOutputBoundary;
     }
 
     @Override
     public void execute() {
-        // TODO: implement the logic of the Logout Use Case
+        final String username = userDataAccessObject.getCurrentUsername();
+        userDataAccessObject.setCurrentUsername(null);
+        final LogoutOutputData logoutOutputData = new LogoutOutputData(username);
+        logoutPresenter.prepareSuccessView(logoutOutputData);
         // * set the current username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         // * tell the presenter to prepare a success view.
